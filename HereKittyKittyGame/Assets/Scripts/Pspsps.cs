737b6c686b4038aba57e15fdf114ccd19691b18b
Pamela Payad <payadpamela@gmail.com>
2020-01-27 22:13:04 -0500

    first iteration of pspsps

---
+++ HereKittyKittyGame/Assets/Scripts/Pspsps.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pspsps : MonoBehaviour
+{
+
+    public Rigidbody2D catRB;
+    public float thrust;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+
+            PspRight();
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+
+            PspsLeft();
+        }
+    }
+
+    void PspRight ()
+    {
+        
+        catRB.AddForce(transform.right * thrust);
+        Debug.Log("psps");
+    }
+
+    void PspsLeft()
+    {
+
+        catRB.AddForce(-transform.right * thrust);
+        Debug.Log("psps");
+    }
+
+}
